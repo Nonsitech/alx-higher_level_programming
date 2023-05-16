70bb544984ada09c689fcdc8f127713d8b0dd8ac
Nonsitech <samleeu01@gmail.com>
2023-05-15 17:10:58 -0700

    6-print_sorted_dictionary.py

--- 0x03-python-data_structures/6-print_sorted_dictionary.py
+++ 0x03-python-data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-def print_sorted_dictionary(a_dictionary):
-    list_ord = list(a_dictionary.keys())
-    list_ord.sort()
-    for i in list_ord:
-        print("{}: {}".format(i, a_dictionary.get(i)))
+
+def print_matrix_integer(matrix=[[]]):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print("{:d}".format(matrix[i][j]), end="")
+            if j != (len(matrix[i]) - 1):
+                print(" ", end="")
+
+        print("")
