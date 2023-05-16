c96c3bec976bc320777fcca01a485322dd71b00a
Nonsitech <samleeu01@gmail.com>
2023-05-15 17:09:04 -0700

    4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
-def only_diff_elements(set_1, set_2):
-    return (set_1 ^ set_2)
+
+def new_in_list(my_list, idx, element):
+
+    if (idx < 0) or (idx > (len(my_list)-1)):
+        return my_list
+
+    copy = [x for x in my_list]
+    copy[idx] = element
+    return copy
