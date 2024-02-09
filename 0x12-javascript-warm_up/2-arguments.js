406e08c1b4e792802995925414449172bd68e8b7
Nonsitech <samleeu01@gmail.com>
2024-02-09 02:04:15 -0800

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,13 @@
 #!/usr/bin/node
-const count = process.argv.length;
-console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
+
+// Get the number of command-line arguments
+const numArguments = process.argv.length - 2;
+
+// Print a message based on the number of arguments
+if (numArguments === 0) {
+  console.log("No argument");
+} else if (numArguments === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
