3774a8cbc6f64f17a41a3067d829bdf3510233f7
Nonsitech <samleeu01@gmail.com>
2024-02-09 02:09:05 -0800

    4-concat.js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,2 +1,8 @@
 #!/usr/bin/node
-console.log(`${process.argv[2]} is ${process.argv[3]}`);
+
+// Get the two arguments from command-line arguments
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+// Print the arguments in the specified format
+console.log(`${arg1} is ${arg2}`);
