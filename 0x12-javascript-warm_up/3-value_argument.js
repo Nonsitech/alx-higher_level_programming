46bd121ab8d00cf560de6f168883794ae08e3771
Nonsitech <samleeu01@gmail.com>
2024-02-09 02:08:07 -0800

    3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,2 +1,7 @@
 #!/usr/bin/node
-console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
+
+// Get the first argument from command-line arguments
+const firstArgument = process.argv[2];
+
+// Print the first argument or "No argument" if none is provided
+console.log(firstArgument ? firstArgument : "No argument");
