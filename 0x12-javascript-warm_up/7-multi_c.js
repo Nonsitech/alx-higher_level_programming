31e40959644be44c26c6aca571abb218109aac27
Nonsitech <samleeu01@gmail.com>
2024-02-09 02:42:32 -0800

    7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,18 @@
 #!/usr/bin/node
-const x = Math.floor(Number(process.argv[2]));
-if (isNaN(x)) {
-  console.log('Missing number of occurrences');
-} else {
-  for (let i = 0; i < x; i++) {
-    console.log('C is fun');
+
+// Get the first argument from command-line arguments
+const arg = process.argv[2];
+
+// Convert the argument to an integer
+const numOccurrences = parseInt(arg);
+let i;
+
+// Check if the conversion is successful
+if (!isNaN(numOccurrences)) {
+  // Loop to print "C is fun" x times
+  for (i = 0; i < numOccurrences; i++) {
+    console.log("C is fun");
   }
+} else {
+  console.log("Missing number of occurrences");
 }
