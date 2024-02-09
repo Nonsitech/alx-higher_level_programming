fc2ba335449e59ed24cf043935fcddd3c4df7bd3
Nonsitech <samleeu01@gmail.com>
2024-02-09 01:58:52 -0800

    1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
-console.log('C is fun');
-console.log('Python is cool');
-console.log('JavaScript is amazing');
+
+// Print three lines using console.log(...)
+console.log("C is fun");
+console.log("Python is cool");
+console.log("JavaScript is amazing");
