df1f5c0ce05dc1354e5f6afac01932b73ac90976
Nonsitech <samleeu01@gmail.com>
2024-02-09 02:13:49 -0800

    5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,3 +1,14 @@
 #!/usr/bin/node
-const num = Math.floor(Number(process.argv[2]));
-console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`);
+
+// Get the first argument from command-line arguments
+const arg = process.argv[2];
+
+// Convert the argument to an integer
+const convertedNumber = parseInt(arg);
+
+// Check if the conversion is successful
+if (!isNaN(convertedNumber)) {
+  console.log(`My number: ${convertedNumber}`);
+} else {
+  console.log("Not a number");
+}
